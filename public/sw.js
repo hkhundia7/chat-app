01ada23c845bdbaec7985b6800ee39371de06ff0
hkhundia <hkhundia6@gmail.com>
2026-06-06 16:42:31 +0530

    add PWA to index

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'chat-app-v1';
+const urlsToCache = ['/', '/login.html', '/register.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
